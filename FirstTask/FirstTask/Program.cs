b8a786006301d8eb77d2836105618130cc1af22a
Esraa2000 <74350522+Esraa2000@users.noreply.github.com>
2025-06-17 01:27:23 +0300

    Update Program.cs

--- FirstTask/FirstTask/Program.cs
+++ FirstTask/FirstTask/Program.cs
@@ -2,19 +2,23 @@
 using FirstTask.Endpoints;
 
 
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 
 app.MapCategoryEndpoints();
 app.MapTagEndpoints();
 app.MapSearchEndpoints();
 app.MapPostEndpoints();
-app.UseStaticFiles();
+
+
+
 
 
 app.Run();
-
 
 
